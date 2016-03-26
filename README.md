6a662d339d5ebbc0bbccf4669aecb1dbc5f0e994
Zachary <zcleary1@ksu.edu>
2016-03-26 14:21:38 -0500

    Initial Commit
    Project Structure created:
    1. Node Modules/Deps installed
    2. Folder Hierarchy created
    3. Git/.gitignore initialized
    4. Initial ReadMe created
    5. Package.Json set up

---
+++ README.md
@@ -0,0 +1,4 @@
+# students-helping-students
+# by Zachary Cleary, Daniel Bamba, and Sagar Mehta
+
+A Question-and-Answer application featuring a live chat option.
---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+  app = express(),
+  favicon = require('serve-favicon'),
+  handlebars = require('express-handlebars').create({ defaultLayout:'main' }),
+  sessions = require('client-sessions');
+// var db = require('./database/seed');
+
+app.disable('x-powered-by');
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+app.use(favicon(__dirname + '/public/img/favicon.png'));
+app.use(sessions({
+  cookieName: 'session',
+  secret: 'somerandomstring',
+  duration: 24*60*60*1000,
+  activeDuration: 1000*60*5
+}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/helpers'));
+
+app.listen(app.get('port'), function(){
+  console.log('Express started. Server listening on port 3000. Press Ctrl-C to terminate');
+});
